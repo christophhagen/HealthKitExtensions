88625ad3480ceea974e47f61e1abb05a84896c73
Christoph Hagen <github@christophhagen.de>
2024-03-06 14:20:14 +0100

    Remove comments from package

--- Package.swift
+++ Package.swift
@@ -1,20 +1,15 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "HealthKitExtensions",
     platforms: [.macOS(.v13), .iOS(.v15), .watchOS(.v9)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HealthKitExtensions",
             targets: ["HealthKitExtensions"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "HealthKitExtensions"),
         .testTarget(
